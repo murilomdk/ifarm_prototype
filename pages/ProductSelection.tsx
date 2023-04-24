847f02a3c615eeed0b7512d0762f584e2aa3121c
LucassMota <lcsmota8@outlook.com>
2023-04-24 11:10:39 -0300

    created ProducSelection page

--- pages/ProductSelection.tsx
+++ pages/ProductSelection.tsx
@@ -1,15 +1,14 @@
-import {Image, ScrollView, View} from "react-native";
+import {ScrollView, View} from "react-native";
 import {CustomLabel, CustomSubtitle} from "../utils/common-components";
 import {CustomInput} from "../utils/common-components/components/CustomInput";
 import {theme} from "../utils/styles/theme";
-import {styles} from "../utils/styles/ThemingProvider";
-import {Switch} from "react-native-paper";
+import {Avatar, List, Switch} from "react-native-paper";
 
 export default function ProductSelection() {
     return (
         <>
             <CustomSubtitle text="Fazenda Asa Branca"></CustomSubtitle>
-            <CustomInput placeholder="Busque pelo nome do item" />
+            <CustomInput placeholder="Busque pelo nome do item" style={{marginTop: theme.size.lg}}/>
 
             <View style={{marginTop: theme.size.xr, borderStyle: 'solid', borderWidth: 1,
                 borderRadius: 10,
@@ -18,74 +17,48 @@ export default function ProductSelection() {
             }}>
                 <CustomLabel variant="titleMedium" text="Frango e derivados" style={{fontWeight: 'bold'}} />
                 <View style={{paddingVertical: theme.size.md, flexDirection: "row", justifyContent: 'space-between'}}>
-                    <CustomLabel text="Item" />
-                    <CustomLabel text="Status" />
+                    <CustomLabel text="Item" style={{paddingHorizontal: theme.size.md}} />
+                    <CustomLabel text="Status" style={{paddingHorizontal: theme.size.lg}} />
                 </View>
                 <ScrollView>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
-                    <View style={{marginTop: theme.size.md, flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Image source={require("../assets/signin-store.png")} style={styles.itemPic}></Image>
-                            <CustomLabel text="Frango de corte" style={{ marginLeft: theme.size.md}} />
-                        </View>
-                        <Switch />
-                    </View>
+                    {items.map((o, key) => {
+                         return (
+                             <List.Item key={key}
+                                        title={o}
+                                        descriptionStyle={{textAlign: 'center'}}
+                                 left={() => (
+                                     <Avatar.Icon size={50}
+                                                  icon={require("../assets/signin-store.png")}
+                                                  style={{backgroundColor: "#ffffff"}}
+                                     />
+                                 )}
+                                        right={() => (
+                                            <Switch value={switched} color={theme.colors.primary}/>
+                                        )}
+                             />
+                         )
+                    })}
                 </ScrollView>
-
             </View>
         </>
     )
 }
 
+let switched = true;
+
 const items = [
+    'frango de corte',
+    'Ovos caipira',
+    'Leite',
+    'Queijo',
+    'frango de corte',
+    'Ovos caipira',
+    'Leite',
+    'Queijo',
+    'frango de corte',
+    'Ovos caipira',
+    'Leite',
+    'Queijo',
     'frango de corte',
     'Ovos caipira',
     'Leite',
