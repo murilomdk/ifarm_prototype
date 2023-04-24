0eeabd5a49db1cb9e14c716389eeceb4e254ca6b
Murilo <murilokiyomura@live.com>
2023-04-24 18:10:06 -0300

    imports app.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,17 @@
-import { StyleSheet, SafeAreaView, Platform, StatusBar } from "react-native";
-import SignInFormPage from "./pages/SignInFormPage";
-import { useTheme } from "react-native-paper";
-import { ThemeInitilizer } from "./utils/styles/ThemingProvider";
-import ProfileSignInPage from "./pages/ProfileSignInPage";
+import {Platform, SafeAreaView, StatusBar, StyleSheet} from "react-native";
+import {ThemeInitilizer} from "./utils/styles/ThemingProvider";
 import NewNegotiationPage from "./pages/NewNegotiation";
-import LoginPage from "./pages/LoginPage";
-import { theme } from "./utils/styles/theme";
+import {theme} from "./utils/styles/theme";
 // import ProductSelection from "./pages/ProductSelection";
-import PropertySelection from "./pages/PropertySelection";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import ProductSelection from "./pages/ProductSelection";
+import {NavigationContainer} from "@react-navigation/native";
 import {TabNavigation} from "./utils/common-components/components/TabNavigation";
 
-const Tab = createBottomTabNavigator();
-
 export default function App() {
-  // const theme = useTheme();
   return (
     <NavigationContainer>
       <ThemeInitilizer>
         <SafeAreaView style={styles.container}>
-          <NewNegotiationPage/>
+          {/*<NewNegotiationPage/>*/}
           <TabNavigation/>
         </SafeAreaView>
       </ThemeInitilizer>
